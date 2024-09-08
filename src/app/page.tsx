54f99ed27a847776e7bdf82f6da0812d5e092a6a
Zrnited <kolamayh@gmail.com>
2024-09-08 07:36:12 +0100

    Completed homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { MouseEventHandler, useState } from "react";
+import { useState } from "react";
+import { products } from "@/utils/Products";
 import Image from "next/image";
 import Layout from "@/components/layout";
 import NextVideo from "next-video";
@@ -12,7 +13,6 @@ import professionalism from "@/assets/icons/professional.png";
 import topLadies from "@/assets/icons/people.png";
 
 export default function Home() {
-  
   const experience = [
     {
       img: fastDelivery,
@@ -36,17 +36,12 @@ export default function Home() {
     },
   ];
 
-  const [productVisibility, setProductVisibility] = useState({
-    bags: true,
-    beadwares: false,
-    partnerProdts: false
-  })
+  const [currentGroup, setCurrentGroup] = useState<string>("");
+  const [getProductId, setGetProductId] = useState<number>();
 
-  // function getCurrentProduct ({e}: object){
-  //   console.log(e)
-  // }
-
-  // console.log(productVisibility);
+  function setProductId (e: number){
+    console.log(e);
+  }
 
   return (
     <Layout>
@@ -67,6 +62,7 @@ export default function Home() {
                 className="object-fill bg-cover h-full w-full"
                 accentColor="#CF8292"
                 src={productVideo}
+                // playbackId="zjrRzADAASQh00XbS00zcE6vwVnirRdbVWi017yP2l02iuw"
               />
             </div>
             <Image
@@ -127,24 +123,103 @@ export default function Home() {
           })}
         </div>
       </section>
-      <section className="py-10 px-5">
+      <section className="pt-10 pb-10 px-5 lg:pt-0 lg:pb-10 xl:px-2">
         <h1 className="text-center text-3xl font-semibold md:text-4xl lg:text-5xl">
           Explore our cutesy products
         </h1>
-        <div className="flex flex-row justify-center gap-5 font-semibold text-lg my-7 border-b border-black">
-          <div className="flex flex-col gap-y-1">
-            <p className="cursor-pointer">Bags</p>
-            {productVisibility.bags && (<span className="bg-black h-[5px] rounded-sm"></span>)}
-          </div>
-          <div className="flex flex-col gap-y-1">
-            <p className="cursor-pointer">Beadwares</p>
-            {productVisibility.beadwares && (<span className="bg-black h-[5px] rounded-sm"></span>)}
-          </div>
-          <div className="flex flex-col gap-y-1">
-            <p className="cursor-pointer">Partners Products</p>
-            {productVisibility.partnerProdts && (<span className="bg-black h-[5px] rounded-sm"></span>)}
+        <div className="flex flex-col justify-center w-full">
+          <div className="flex flex-row justify-center gap-5 font-semibold text-lg my-7 border-b border-black lg:max-w-[400px] lg:place-self-center">
+            <div className="flex flex-col gap-y-1">
+              <p
+                onClick={(e: any) => {
+                  // console.log(e.target.innerHTML);
+                  const group = e.target.innerHTML;
+                  setCurrentGroup(group);
+                }}
+                className="cursor-pointer"
+              >
+                Bags
+              </p>
+              {(currentGroup === "Bags" || currentGroup === "") && (
+                <span className="bg-black h-[5px] rounded-sm"></span>
+              )}
+            </div>
+            <div className="flex flex-col gap-y-1">
+              <p
+                onClick={(e: any) => {
+                  // console.log(e.target.innerHTML);
+                  const group = e.target.innerHTML;
+                  setCurrentGroup(group);
+                }}
+                className="cursor-pointer"
+              >
+                Beadwares
+              </p>
+              {currentGroup === "Beadwares" && (
+                <span className="bg-black h-[5px] rounded-sm"></span>
+              )}
+            </div>
+            <div className="flex flex-col gap-y-1">
+              <p
+                onClick={(e: any) => {
+                  // console.log(e.target.innerHTML);
+                  const group = e.target.innerHTML;
+                  setCurrentGroup(group);
+                }}
+                className="cursor-pointer"
+              >
+                Partners Products
+              </p>
+              {currentGroup === "Partners Products" && (
+                <span className="bg-black h-[5px] rounded-sm"></span>
+              )}
+            </div>
           </div>
         </div>
+        {/* Products display section */}
+        <article className="grid grid-cols-1 gap-y-5 justify-center place-items-center sm:grid-cols-2 sm:gap-y-8 sm:gap-x-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+          {products?.map((prod, index) => {
+            return (
+              <div
+                key={index}
+                className="h-[244px] w-[190px] bg-[#E8C3CB] border border-black relative sm:h-[300px] sm:w-[246px] md:h-[480px] md:w-[407px]"
+              >
+                <div className="absolute -right-2 -bottom-2 border border-black bg-white h-[244px] w-[190px] p-1 sm:h-[300px] sm:w-[246px] md:h-[480px] md:w-[407px] md:p-2">
+                  <Image
+                    src={prod.coverImg}
+                    priority
+                    alt="product"
+                    className="w-full h-[138px] sm:h-[185px] md:h-[305px]"
+                  />
+                  <div className="flex flex-col md:flex-row md:justify-between md:items-center md:mt-2">
+                    <div>
+                      <h2 className="text-xl font-semibold md:text-3xl">
+                        {prod.name}
+                      </h2>
+                      <p className="text-xs font-medium md:text-lg">
+                        {prod.desc}
+                      </p>
+                    </div>
+                    <div className="flex flex-row gap-x-1 items-center md:flex-col md:items-end">
+                      <h3 className="font-semibold md:text-3xl">{`N${prod.price}`}</h3>
+                      <p className="text-[10px] text-white bg-[#00A460] px-1 md:text-sm">
+                        {prod.status}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-row justify-between items-center mt-0.5 md:mt-7">
+                    <button onClick={()=>setProductId(prod.id)} className="h-[25px] bg-[#2E2729] text-white text-sm text-center px-2 md:text-lg sm:h-[35px] sm:w-[115px] md:h-[55px] md:w-[188px]">
+                      Shop Now
+                    </button>
+                    <button className="h-[25px] bg-white text-[#2E2729] border-[#2E2729] border text-sm text-center px-2 font-semibold sm:h-[35px] sm:w-[115px] md:text-lg md:h-[55px] md:w-[188px]">
+                      Add to Cart +
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </article>
       </section>
     </Layout>
   );
---
+++ src/utils/Products.tsx
@@ -0,0 +1,78 @@
+import product1 from "@/assets/images/product1.png";
+import product2 from "@/assets/images/product2.png";
+import product3 from "@/assets/images/product3.png";
+import fullImg from "@/assets/images/product3-2.png";
+import product4 from "@/assets/images/product4.png";
+import product5 from "@/assets/images/product5.png";
+import product6 from "@/assets/images/product6.png";
+
+const products = [
+    {
+        id: 1,
+        coverImg: product1,
+        fullImg: fullImg,
+        name: "Curl Hand Bag",
+        desc: "Exotic whiite bag for all occassion",
+        about: "Step into luxury with our bestselling Purple Fur Bag. Crafted from the softest faux fur, this chic accessory is a perfect blend of style and comfort. Its rich purple hue adds a pop of color to any outfit, making it a versatile statement piece for day or night. Spacious enough to carry your essentials, yet compact enough to stay effortlessly elegant, this bag is designed for those who love to stand out. Whether you’re heading to a casual brunch or a glamorous evening out, the Purple Fur Bag is your go-to for adding a touch of opulence to your look.",
+        price: 45000,
+        status: "Available",   
+        Category: "Bags"
+    },
+    {
+        id: 2,
+        coverImg: product2,
+        fullImg: fullImg,
+        name: "Pink Queens Bag",
+        desc: "Exotic pink bag for all my girls",
+        about: "Step into luxury with our bestselling Purple Fur Bag. Crafted from the softest faux fur, this chic accessory is a perfect blend of style and comfort. Its rich purple hue adds a pop of color to any outfit, making it a versatile statement piece for day or night. Spacious enough to carry your essentials, yet compact enough to stay effortlessly elegant, this bag is designed for those who love to stand out. Whether you’re heading to a casual brunch or a glamorous evening out, the Purple Fur Bag is your go-to for adding a touch of opulence to your look.",
+        price: 50000,
+        status: "Available",   
+        Category: "Bags"
+    },
+    {
+        id: 3,
+        coverImg: product3,
+        fullImg: fullImg,
+        name: "Purple Fur Bag",
+        desc: "Our best-selling fur bag so far",
+        about: "Step into luxury with our bestselling Purple Fur Bag. Crafted from the softest faux fur, this chic accessory is a perfect blend of style and comfort. Its rich purple hue adds a pop of color to any outfit, making it a versatile statement piece for day or night. Spacious enough to carry your essentials, yet compact enough to stay effortlessly elegant, this bag is designed for those who love to stand out. Whether you’re heading to a casual brunch or a glamorous evening out, the Purple Fur Bag is your go-to for adding a touch of opulence to your look.",
+        price: 39000,
+        status: "Available",   
+        Category: "Bags"
+    },
+    {
+        id: 4,
+        coverImg: product4,
+        fullImg: fullImg,
+        name: "Black Fur Bag",
+        desc: "Exotic black bag for all occasion",
+        about: "Step into luxury with our bestselling Purple Fur Bag. Crafted from the softest faux fur, this chic accessory is a perfect blend of style and comfort. Its rich purple hue adds a pop of color to any outfit, making it a versatile statement piece for day or night. Spacious enough to carry your essentials, yet compact enough to stay effortlessly elegant, this bag is designed for those who love to stand out. Whether you’re heading to a casual brunch or a glamorous evening out, the Purple Fur Bag is your go-to for adding a touch of opulence to your look.",
+        price: 35000,
+        status: "Available",   
+        Category: "Bags"
+    },
+    {
+        id: 5,
+        coverImg: product5,
+        fullImg: fullImg,
+        name: "Black Portable Purse",
+        desc: "Easy to wear fashionable purse",
+        about: "Step into luxury with our bestselling Purple Fur Bag. Crafted from the softest faux fur, this chic accessory is a perfect blend of style and comfort. Its rich purple hue adds a pop of color to any outfit, making it a versatile statement piece for day or night. Spacious enough to carry your essentials, yet compact enough to stay effortlessly elegant, this bag is designed for those who love to stand out. Whether you’re heading to a casual brunch or a glamorous evening out, the Purple Fur Bag is your go-to for adding a touch of opulence to your look.",
+        price: 60000,
+        status: "Available",   
+        Category: "Bags"
+    },
+    {
+        id: 6,
+        coverImg: product6,
+        fullImg: fullImg,
+        name: "Black Round Purse",
+        desc: "Exotic black purse for all occasion",
+        about: "Step into luxury with our bestselling Purple Fur Bag. Crafted from the softest faux fur, this chic accessory is a perfect blend of style and comfort. Its rich purple hue adds a pop of color to any outfit, making it a versatile statement piece for day or night. Spacious enough to carry your essentials, yet compact enough to stay effortlessly elegant, this bag is designed for those who love to stand out. Whether you’re heading to a casual brunch or a glamorous evening out, the Purple Fur Bag is your go-to for adding a touch of opulence to your look.",
+        price: 50000,
+        status: "Available",   
+        Category: "Bags"
+    },
+]
+
+export {products};
